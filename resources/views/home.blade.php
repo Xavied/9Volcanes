7f78e7626f8a6b7b423831a3d66a2f5cad404441
Alex-Xavier99 <axortiz@puce.edu.ec>
2021-11-10 20:15:49 -0500

    Se modifica la vista home - la vista nostoros

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
 <!-- sección menú -->
 @section('navigation')
-    
+
 @endsection
 <!-- Fin sección menú -->
 
@@ -36,17 +36,28 @@
                 aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner carouseltmn">
+            @foreach ($productos as $producto)
+                <?php
+                $id = $producto->id;
+                if ($id == 1) {
+                    $img1 = $producto->Imagenes->first()->url;
+                } elseif ($id == 2) {
+                    $img2 = $producto->Imagenes->first()->url;
+                } elseif ($id == 3) {
+                    $img3 = $producto->Imagenes->first()->url;
+                    break;
+                }
+                ?>
+            @endforeach
+
             <div class="carousel-item active">
-                <img src="https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_960_720.jpg"
-                    class="d-block imgtmncrsl " alt="Primer Slide">
+                <img src="{{ url('/storage') . '/' . $img1 }}" class="d-block imgtmncrsl " alt="Primer Slide">
             </div>
             <div class="carousel-item">
-                <img src="https://cdn.pixabay.com/photo/2017/07/31/04/11/tomato-2556426_960_720.jpg"
-                    class="d-block imgtmncrsl" alt="Segundo Slide">
+                <img src="{{ url('/storage') . '/' . $img2 }}" class="d-block imgtmncrsl" alt="Segundo Slide">
             </div>
             <div class="carousel-item">
-                <img src="https://cdn.pixabay.com/photo/2018/03/21/23/21/nuts-3248743_960_720.jpg"
-                    class="d-block imgtmncrsl" alt="Tercer Slide">
+                <img src="{{ url('/storage') . '/' . $img3 }}" class="d-block imgtmncrsl" alt="Tercer Slide">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -61,5 +72,52 @@
         </button>
     </div>
     <!--fin del carousel-->
+    <br>
+    <h2 class="text-center txtprom">Promociones</h2>
+    <br>
+    <div class="row gy-3 justify-content-center">
+        @foreach ($promociones as $promocion)
+            <!-- Filtro titulo -->
+            <?php
+            $tmpnom = $promocion->nombre;
+            $tmpdescrp = $promocion->descripcion;
+            $txtpnto = '...';
+            if (strlen($tmpnom) > 14) {
+                $tmpnom = substr($tmpnom, 0, 14) . $txtpnto;
+            }
+            if (strlen($tmpdescrp) > 18) {
+                $tmpdescrp = substr($tmpdescrp, 0, 18) . $txtpnto;
+            }
+            ?>
+            <div class="col-md-4 ">
+                <div class="contenPrd">
+                    <a href="{{ route('producto', $promocion) }}">
+                    <img src="{{ url('/storage') . '/' . $promocion->Imagenes->first()->url }}" class="card-img-top"
+                        alt="...">
+                    </a>
+                    <div class="txtproducto">
+                        <div class="row d-flex align-items-center">
+                            <div class="col">
+                                <div class="ms-2">
+                                    <h5 class="card-title">{{ $promocion->nombre }}</h5>
+                                    <p class="card-text ">{{ $tmpdescrp }}</p>
+                                </div>                                                               
+
+                            </div>
+                            <div class="col d-flex justify-content-end m-2">
+                                <div>
+                                    <p class="card-text text-center mb-0">${{ $promocion->precio }}</p>
+                                    <a href="{{ route('producto', $promocion) }}" class="btn " >Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        @endforeach
+    </div>
     <br>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,15 @@ Route::get('/', function () {
 
 
 //Productos
-Route::get('/productos', [ProductoController::class,'productos']);
-Route::get('/productos/{productoe:slug}', [ProductoController::class,'producto'])->name('producto');
+Route::get('/productos', [ProductoController::class, 'productos']);
+Route::get('/productos/{productoe:slug}', [ProductoController::class, 'producto'])->name('producto');
 
-Route::get('/home',[HomeController::class,'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/emprendimientos', [EmprendimientoController::class, 'index'])->name('emprendimientos.index');
-
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -36,4 +38,4 @@ Route::get('/dashboard', function () {
 
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
